fe241945962314c0e3b9405d8d99f434530243ac
Kunalavghade <67228832+kunalavghade@users.noreply.github.com>
2020-11-14 11:16:42 +0530

    Delete searchWin.py

--- searchWin.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-class SearchWin():
-    def __init__(self,window):
-        self.root = window
-        self.windows = Frame(self.root,bg="blue")
-        self.windows.place(relheight=1,relwidth=1)
-
